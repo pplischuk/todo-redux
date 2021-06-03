ff671d4cccf6d28f7591af9f2dd2d5a660b9ab39
pplischuk <pplischuk@gmail.com>
2021-06-03 21:19:05 +0300

    create app

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Todo from './todoList';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Todo />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
---
+++ src/todoList.js
@@ -0,0 +1,23 @@
+import './todoList.css';
+import moment from 'moment'
+
+function Todo() {
+  return (
+    
+    <div className = 'wrapper'>
+      <div className = 'date'>
+        <div className = 'yearDayMonth'>
+          <div className = 'day'>{moment().format('D')}</div>
+          <div className = 'yearMonth'>
+            <div>{moment().format('MMM')}</div>
+            <div>{moment().format('YYYY')}</div>
+          </div>
+        </div>
+        <div className = 'nameOfDay'>{moment().format('dddd')}</div>
+      </div>
+   
+    </div>
+  );
+}
+
+export default Todo;
