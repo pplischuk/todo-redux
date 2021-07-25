35a5e640e98bb24f656fa7bfe703e5e0eaf57afb
pplischuk <pplischuk@gmail.com>
2021-07-26 00:50:23 +0300

    fix btn styles

--- src/listItem/listItem.js
+++ src/listItem/listItem.js
@@ -14,13 +14,19 @@ class Todos extends React.Component{
 			return a.todosList.map(todo => (
 					<div className = "listItemWrapper" key = {todo.id}>
 						<span className = {todo.isdone ? "listItem_done" : "listItem"}>{todo.content}</span>
-						<button onClick = {() => {
-						this.props.changeIsDone(todo.id);
-					}} className = {todo.isdone ? "btn_done" : "btn"}></button>
-						<button className = "btnDelete"
-						onClick = {() => {
-						this.props.deleteItem(todo.id);
-					}}>&times;</button>
+							<div className = "buttonWrapper">
+								<button
+									onClick = {() => {
+									this.props.changeIsDone(todo.id);
+									}} className = {todo.isdone ? "btn_done" : "btn"}>
+								</button>
+								<button className = "btnDelete"
+									onClick = {() => {
+									this.props.deleteItem(todo.id);
+									}}>
+									&times;
+								</button>
+						</div>
 					</div>
 				));
 		}
